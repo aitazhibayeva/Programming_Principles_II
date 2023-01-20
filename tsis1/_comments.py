87d7f61e17428df15f5022a48d7f5336a22c271f
ai_tazhibayeva <115732176+aizadatazhibaeva@users.noreply.github.com>
2023-01-20 22:56:36 +0600

    python

---
+++ tsis1/_comments.py
@@ -0,0 +1,25 @@
+#Ex1
+#This is a comment
+print("Hello, World!")
+
+#Ex2
+print("Hello, World!") #This is a comment
+
+#Ex3
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Ex4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+#Ex5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
