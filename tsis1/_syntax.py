f3864812f7000991929b2f6fc9b578ee3417b6b8
ai_tazhibayeva <115732176+aizadatazhibaeva@users.noreply.github.com>
2023-01-20 22:51:36 +0600

    python

---
+++ tsis1/_syntax.py
@@ -0,0 +1,20 @@
+#Ex1
+if 5 > 2:
+  print("Five is greater than two!")
+  
+  
+#Ex2
+if 5 > 2:
+  print("Five is greater than two!") 
+if 5 > 2:
+  print("Five is greater than two!") 
+  
+  
+#Ex3
+x = 5
+y = "Hello, World!"
+
+
+#Ex4
+#This is a comment.
+print("Hello, World!")
