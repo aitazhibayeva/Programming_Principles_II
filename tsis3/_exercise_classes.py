6dafbe4d1358307a60a8bd0a9f078108faf932c2
aitazhibayeva <tazhibayevaaiza@gmail.com>
2023-02-06 23:04:10 +0600

    week3

--- tsis3/_exercise_classes.py
+++ tsis3/_exercise_classes.py
@@ -127,3 +127,31 @@ acnt.withdraw(75)
 acnt.withdraw(500)
 
 
+
+#Ex6
+def IsPrime(num):
+    if num == 0 or num == 1:
+        return False
+    cnt = True
+    for i in range(2, num):
+        if(num % 2 == 0):
+            cnt = False 
+    if(cnt):
+        return True 
+    else:
+        return False 
+    
+def filter_prime(all_num):
+    prime_num = list()
+    for i in all_num:
+        if(IsPrime(i)):
+            prime_num.append(i)
+    return prime_num
+
+
+size_list = int(input())
+list_1 = list()
+for i in range(size_list):
+    list_1.append(int(input()))
+
+print(filter_prime(list_1))
