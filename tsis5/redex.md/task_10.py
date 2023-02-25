c5be847a2598c09edc61e9dd2ad2fcfb6a412195
aitazhibayeva <tazhibayevaaiza@gmail.com>
2023-02-26 01:06:07 +0600

    week5

--- tsis5/redex.md/task_10.py
+++ tsis5/redex.md/task_10.py
@@ -1,13 +1,9 @@
 #Write a Python program to convert a given camel case string to snake case.
 import re
 
-def snakeСase(text):
-    text = [text[0].upper()]
-    pattern = r'[A-Z]{1}[a-z]*'
-    tt = re.findall(pattern, text).lower()
-    text = '_'.join(tt)
-    text = [x.lower() for x in text]
-    return text
+def change_case(str):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)
+    print(s1)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-
-print(snakeСase("camelCaseString"))
+print(change_case("geeksForGeeks"))
