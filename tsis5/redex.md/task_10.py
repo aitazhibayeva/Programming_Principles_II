2228e1ec101cb9933d55be96f5f9bba6bcbc13ab
aitazhibayeva <tazhibayevaaiza@gmail.com>
2023-03-07 12:31:07 +0600

    week5

--- tsis5/redex.md/task_10.py
+++ tsis5/redex.md/task_10.py
@@ -2,8 +2,6 @@
 import re
 
 def change_case(str):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)
-    print(s1)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str).lower()
 
-print(change_case("geeksForGeeks"))
+print(change_case("camelCaseString"))
